d950f5969d14b8468828d17b393a0f0bf0ecce5d
redvatsu-glitch <redvatsu@gmail.com>
2026-05-03 00:43:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2rqMXYJsePhCYav_vjIxpsOFcQ1_Uyw8",
-  authDomain: "copy-shield.firebaseapp.com",
-  projectId: "copy-shield",
-  storageBucket: "copy-shield.firebasestorage.app",
-  messagingSenderId: "320614961119",
-  appId: "1:320614961119:web:6ba332a3ade364d5ef7e6a",
-  measurementId: "G-WP3VBPCRBQ"
+ 
 };
